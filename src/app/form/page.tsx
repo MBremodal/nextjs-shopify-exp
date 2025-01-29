81021134a90424d43a32f0205f6af79652ef2176
Artur <mixmln14@gmail.com>
2025-01-29 21:02:36 +0100

    button

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -7,8 +7,7 @@ export default async function Form() {
 	};
 
 	return (
-		<div className='w-[500px] mx-auto'>
-			<h1>Form</h1>
+		<div className='w-[500px] mx-auto border border-gray-300 p-8 rounded-md bg-slate-100'>
 			<form
 				action={handleSubmit}
 				className='flex flex-col gap-4'>
@@ -26,7 +25,11 @@ export default async function Form() {
 					type='file'
 					name='file'
 				/>
-				<button type='submit'>Submit</button>
+				<button
+					type='submit'
+					className='border border-orange-500 py-2 rounded-md text-orange-500 hover:bg-orange-500 hover:text-white transition-colors'>
+					Submit
+				</button>
 			</form>
 		</div>
 	);
